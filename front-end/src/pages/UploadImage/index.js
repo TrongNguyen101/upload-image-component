e02484d70d4b2f1051c422934f9c6f7b7f0bad40
Nguyen Trong <nguyentrong.se11@gmail.com>
2025-01-09 08:48:43 +0700

    refactor front-end UploadImage page

--- front-end/src/pages/UploadImage/index.js
+++ front-end/src/pages/UploadImage/index.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import * as UploadService from "../../services/UploadService";
 
 function UploadImagePage() {
+  const navigate = useNavigate();
   const [seleactedFile, setSelectedFile] = useState(null);
   const [preview, setPreview] = useState(null);
   const formData = new FormData();
@@ -28,26 +30,45 @@ function UploadImagePage() {
     }
   };
 
+  const handleBackHome = () => {
+    navigate("/");
+  };
+
   return (
-    <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
-      <div
-        style={{
-          border: "1px solid black",
-          display: "flex",
-          alignItems: "center",
-          borderRadius: "5px",
-          paddingLeft: "5px",
-        }}
-      >
-        <input type="file" accept="image/*" onChange={handleFileChange} />
+    <div>
+      <div style={{ display: "flex", flexDirection: "row", gap: "1rem" }}>
+        <div
+          style={{
+            width: "380px",
+            border: "1px solid black",
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "5px",
+            paddingLeft: "5px",
+          }}
+        >
+          <input
+            style={{ width: "100%" }}
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </div>
+        <div>
+          <Button variant="contained" onClick={handleUpload}>
+            upload
+          </Button>
+        </div>
+        <div>
+          <Button variant="contained" onClick={handleBackHome}>
+            back home
+          </Button>
+        </div>
+      </div>
+      <div style={{ marginTop: "1rem" }}>
         {preview && (
           <img src={preview} alt="preview" style={{ width: "100px" }} />
         )}
-      </div>
-      <div>
-        <Button variant="contained" onClick={handleUpload}>
-          upload
-        </Button>
       </div>
     </div>
   );
